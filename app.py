e092dd0f2cf80ce63a0bb1dd4eb5c0e9dddd5985
Sanjeev Hari <92075538+Sanjeevhari@users.noreply.github.com>
2024-04-01 12:36:22 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Retinal Disease Detection",
+    page_icon = ":eye:",
+    initial_sidebar_state = 'auto'
+)
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+with st.sidebar:
+        st.image('mg.png')
+        st.title("Mangifera Healthika")
+        st.subheader("Detection of diseases present in the Retinal. This helps an user to easily detect the disease and identify it's cause.")
